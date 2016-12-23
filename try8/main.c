506aae0b9ef53e7cce66eccef990e1cf9d38a5b7
Erik Svedäng <erik.svedang@gmail.com>
2016-12-23 09:59:21 +0100

    Can draw two different shapes.

--- try8/main.c
+++ try8/main.c
@@ -39,12 +39,37 @@ int main() {
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
     glEnableVertexAttribArray(0);
     
+    // More data to render
+    float data2[] = {
+        -0.35, 0.5,
+        0.75, 0.75,
+        0.0, -0.5
+    };
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    GLuint vao2;
+    glGenVertexArrays(1, &vao2);
+    glBindVertexArray(vao2);
+    
+    GLuint vbo2;
+    glGenBuffers(1, &vbo2);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(data2), data2, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    
     int run = 1;
     while(run) {         
         // Render Loop
         glClearColor(0.2, 0.2, 0.2, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindVertexArray(vao);
         glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(vao2);
+        glDrawArrays(GL_LINE_STRIP, 0, 3);
 
         // Show!
         SDL_GL_SwapWindow(glap.window);
